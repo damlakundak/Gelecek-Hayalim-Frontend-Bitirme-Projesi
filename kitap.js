580312a65011f685282018509406924fd1ec87f9
aysegulsum <aysegulsumeren@gmail.com>
2024-10-11 20:37:49 +0300

    kitap ve film navbar düzenlemesi

--- kitap.js
+++ kitap.js
@@ -11,15 +11,15 @@ const radioButtons = document.querySelectorAll('input[name="slider"]');
 let currentIndex = 0;
 
 function updateSlider(index) {
-    radioButtons[index].checked = true;
+  radioButtons[index].checked = true;
 }
 
 prevButton.addEventListener("click", () => {
-    currentIndex = (currentIndex - 1 + radioButtons.length) % radioButtons.length;
-    updateSlider(currentIndex);
+  currentIndex = (currentIndex - 1 + radioButtons.length) % radioButtons.length;
+  updateSlider(currentIndex);
 });
 
 nextButton.addEventListener("click", () => {
-    currentIndex = (currentIndex + 1) % radioButtons.length;
-    updateSlider(currentIndex);
+  currentIndex = (currentIndex + 1) % radioButtons.length;
+  updateSlider(currentIndex);
 });
