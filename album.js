2d26ee60503a243f9a660979d3d2aebe8e4954b9
Ayşe Gülsüm EREN <aysegulsumeren@gmail.com>
2024-10-13 23:55:33 +0300

    yorumlar

--- album.js
+++ album.js
@@ -1,3 +1,8 @@
+/*
+ * This script handles the focus behavior of an input element within a modal.
+ * When the modal with the ID "myModal" is shown, the input element with the ID "myInput"
+ * will automatically receive focus.
+ */
 const myModal = document.getElementById("myModal");
 const myInput = document.getElementById("myInput");
 
