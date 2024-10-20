559e1d7efc43cbcda13e6a3ae4c110887938665f
damlakundak <159939168+damlakundak@users.noreply.github.com>
2024-10-20 12:01:46 +0300

    kitap.js son hali

--- kitap.js
+++ kitap.js
@@ -1,27 +1,58 @@
-// "prev" butonunu seçer
+// Slider buton ve radio butonu
 const prevButton = document.getElementById("prev");
-// "next" butonunu seçer
 const nextButton = document.getElementById("next");
 const radioButtons = document.querySelectorAll('input[name="slider"]');
 
-// Mevcut indeks (slayt) için başlangıç değeri
+// Mevcut index (başlangıç slide'ı)
 let currentIndex = 0;
 
+// Slider'ı mevcut index'e göre güncelleyen fonksiyon
 function updateSlider(index) {
-  // Belirtilen indekse sahip radio butonunu seçili yapar
   radioButtons[index].checked = true;
 }
 
-// Önceki butonuna tıklama olayı
 prevButton.addEventListener("click", () => {
-  // Mevcut indeksi bir azaltır, döngüsel olması için toplam buton sayısı kadar ekler
+ // Index'i azalt ve döngüsel hale getir (ilk slide'a dön)
   currentIndex = (currentIndex - 1 + radioButtons.length) % radioButtons.length;
-  // Slaytı günceller
   updateSlider(currentIndex);
 });
 
 nextButton.addEventListener("click", () => {
-  // Mevcut indeksi bir arttırır, döngüsel olması için radio butonlarının sayısını kullanır
+   // Index'i artır ve döngüsel hale getir (son slide'dan sonra başa dön)
   currentIndex = (currentIndex + 1) % radioButtons.length;
   updateSlider(currentIndex);
 });
+
+// Tema değiştirme (dark/light)
+document.getElementById("hide-checkbox").addEventListener("change", function () {
+  if (this.checked) {  // Eğer checkbox işaretli ise light tema uygula
+    document.body.classList.add("light-theme");
+    document.body.classList.remove("dark-theme");
+  } else {   // Eğer checkbox işaretli değilse dark tema uygula
+    document.body.classList.add("dark-theme");
+    document.body.classList.remove("light-theme");
+  }
+});
+
+// Typewriter (yazı yazma) efekti
+document.addEventListener("DOMContentLoaded", function () {
+  typeWriter();
+});
+
+let i = 0;
+const txt =
+  "İş dünyasında zorluklara göğüs geren kadınların ilham verici hikayelerini keşfedin. Bu platform, engelleri aşarak başarıya ulaşan güçlü kadınların deneyimlerini ve mücadelelerini paylaşmak amacıyla oluşturuldu. Her bir hikaye, azim, kararlılık ve yenilikçilikle dolu. Kendinizi bu cesur kadınların yolculuklarına kaptırın, onların deneyimlerinden ilham alın ve kendi potansiyelinizi keşfedin. Çünkü başarı, cesaretle başlar!";
+const speed = 25;  // Yazma hızı (milisaniye)
+
+function typeWriter() {
+  if (i < txt.length) {  // Metindeki harfleri sırayla yazdır
+    document.getElementById("typing").innerHTML += txt.charAt(i);
+    i++;
+   // Eğer bir nokta (cümle sonu) varsa 0.5 saniye durakla
+    if (txt.charAt(i - 1) === ".") {
+      setTimeout(typeWriter, 500);
+    } else {
+      setTimeout(typeWriter, speed);
+    }
+  }
+}
